ab8cc76dc3e3562771243efb27de56979bac6ec1
Fabio Guilherme <fabio.guilherme@gmail.com>
2022-05-18 12:03:51 +0100

    Updating credentials for PG (Heroku).

--- models/pg/connection.js
+++ models/pg/connection.js
@@ -1,8 +1,8 @@
 var pg = require('pg');
 
-//const connectionString = "postgres://frqlcxbdpnqpbx:6490e71efb2582596c194121df35398e720d9dd0db63a21b23675f58e08dbb66@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d3u6q577hiruhf"
-const connectionString = "postgres://postgres:password@localhost:5432/Products"
-    //const connectionString = "postgres://" + process.env.DBUSER + ":" + process.env.DBPASS + "@localhost:5432/Products"
+const connectionString = "postgres://wggbajyylhlltm:02bfee0d3f8e4a72070237e2132da59a2166196ff39d1b7f8b1f4ccb7fd48fae@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/d2blnpac0dv643";
+//const connectionString = "postgres://postgres:password@localhost:5432/Products";
+//const connectionString = "postgres://" + process.env.DBUSER + ":" + process.env.DBPASS + "@localhost:5432/Products";
 console.log("connectionString = " + connectionString);
 
 const Pool = pg.Pool
